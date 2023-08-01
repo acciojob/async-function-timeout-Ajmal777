7545995dee0239d9cf159e366c1a41a9b7560fb6
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-08-01 17:58:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+const textField = document.getElementById('text');
+const delayField = document.getElementById('delay');
+const btn = document.getElementById('btn');
+const div = document.getElementById('output');
+
+btn.addEventListener('click', ()=>{
+	const text = textField.value;
+	const delay = delayField.value;
+	display(text, delay);
+})
+
+async function display(text, delay){
+	setTimeout(()=>{
+		div.innerText = text;
+	}, delay);
+}
